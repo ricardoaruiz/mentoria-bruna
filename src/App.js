e8d8dc82eb2e27b1d96332943a4ce20ef47f447f
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2022-03-17 17:09:47 -0300

    feat: create Home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { HomePage } from './pages';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      
+      <HomePage />
+
     </div>
   );
 }
