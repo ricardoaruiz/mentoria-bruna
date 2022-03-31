640b795d10a4d992a2620658d8bbe72389e7f3d0
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2022-03-31 16:24:15 -0300

    feat: create fake backend and partial refactor on Home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,22 +3,37 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { InputTask } from '../../components/InputTask';
 import { ListTask } from '../../components/ListTask';
-import { TASKS } from '../../constants/data'
 
 import './styles.css'
 
 export const HomePage = () => {
 
   const [task, setTask] = React.useState('')
-  const [tasks, setTasks] = React.useState(TASKS)
+  const [tasks, setTasks] = React.useState([])
 
   const handleTaskInputChange = (event) => {
     setTask(event.target.value)
   }
 
-  const handleTaskAddButtonClick = () => {    
-    setTasks(currentTasks => [ ...currentTasks, { id: uuidv4(), description: task } ])
-    setTask('')
+  const handleTaskAddButtonClick = async () => {
+
+    try {
+      const newTask = { id: uuidv4(), description: task, status: 'pending' }
+  
+      await fetch('http://localhost:9000/taks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTask)
+      })
+  
+      setTasks(currentTasks => [ ...currentTasks, newTask ])
+      setTask('')
+    } catch(error) {
+      // TODO: tratar o erro de forma mais bonita
+      console.log('Xiii deu erro no server ao adicionar uma nova task', error)
+    }
   }
 
   const handleRemoveItemList = (id) => {
@@ -36,6 +51,21 @@ export const HomePage = () => {
 
     setTasks(newTasks)
   }
+
+  const loadTasks = async () => {
+    try {
+      const response = await fetch('http://localhost:9000/taks')
+      const tasks = await response.json()
+      setTasks(tasks)
+    } catch(error) {
+      // TODO: tratar o erro de forma mais bonita
+      console.log('Xiii deu erro no server ao consultar as tasks', error)
+    }
+  }
+
+  React.useEffect(() => {
+    loadTasks()
+  }, [])
 
   return (
     <div className="home-container">
