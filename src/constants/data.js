54979ea236179c7daafd35b1b360719e2586d86a
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2022-03-31 16:26:40 -0300

    refactor: removing constants folder

--- src/constants/data.js
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-
-export const TASKS = [
-  { id: uuidv4(), description: 'Task #1', status: 'pending' },
-  { id: uuidv4(), description: 'Task #2', status: 'pending' },
-  { id: uuidv4(), description: 'Task #3', status: 'pending' },
-  { id: uuidv4(), description: 'Task #4', status: 'pending' },
-  { id: uuidv4(), description: 'Task #5', status: 'pending' },
-]
